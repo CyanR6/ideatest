6d8d2d32ded0854fc46b193b6dc59a9e4c556d1a
cyan <931655778@qq.com>
2019-08-10 16:14:15 +0800

    1

---
+++ ideatest1/src/com/system/test1.java
@@ -0,0 +1,17 @@
+package com.system;
+
+/**
+ * @author admin
+ * @create 2019-08-10 9:12
+ */
+public class test1 {
+
+    public static void main(String[] args) {
+
+        System.out.print("eee");
+    }
+
+    void maset() {
+
+    }
+}
